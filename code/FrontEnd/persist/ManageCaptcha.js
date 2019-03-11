b0da64d96d2d936b615f98cc8f098b4aacc43525
Marcel Pereira Rebello <marcel.rebello@br.unisys.com>
2019-03-11 18:30:02 -0300

    include skip to dev

--- code/FrontEnd/persist/ManageCaptcha.js
+++ code/FrontEnd/persist/ManageCaptcha.js
@@ -8,6 +8,7 @@ class ManageCaptcha {
     this._config_client_side = config.authentication.google_recaptcha_client_side;
     this._config_site_to_verify = config.authentication.google_verify_site;
     this._config_proxy = config.authentication.proxy_to_access;
+    this._config_auth_mode = config.authentication.mode;
     this._client_answer = '';
   }
 
@@ -19,6 +20,10 @@ class ManageCaptcha {
     this._client_ip = req.connection.remoteAddress;
     this._client_answer = req.body.recaptcha_response;
     return new Promise ((resolve, reject) => {
+      if (this._config_auth_mode == 'desenv'){
+        return resolve('desenv mode');
+      }
+
       if (this._client_answer == '' ){
         return reject ('Erro no Captcha');
       }
